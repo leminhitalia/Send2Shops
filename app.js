91810ff4d416a3effb8dc797737cbe553104d261
ymle <leminhitalia@gmail.com>
2017-09-19 03:12:44 +0700

    v1.0.0

---
+++ app.js
@@ -0,0 +1,38 @@
+var fs = require('fs');
+
+//read list of shops
+var shops = fs.readFileSync('./shops.txt').toString().split("\n");
+
+//read a message
+var message = fs.readFileSync('./messages.txt','utf8').toString();
+
+//Read account
+var account = require('./account');
+var email = account.email;
+var password = account.password;
+
+//facebook-chat-api
+var login = require("facebook-chat-api");
+
+login({email: email, password: password}, (err, api) => {
+    if(err) {
+        return console.error(err);
+    }
+   
+
+    for(i in shops){
+        var shopName = shops[i];
+        console.log("Shop: " + shopName);
+        
+        api.getUserID(shopName, (err, data) => {
+            
+            if(err) {
+                return console.error(err);
+            }
+            
+            var receptionID = data[0].userID;
+            console.log("Msg: " + message);
+            api.sendMessage(message, receptionID);
+        });
+    }
+});
